ece31063cc5d9478f92794a68519394933d8d0b5
wtwver <wtwver>
2025-03-04 00:18:22 +0800

    py3.9 and numpy2.0

--- code/run_sl.sh
+++ code/run_sl.sh
@@ -1,4 +1,4 @@
 #uv venv --python=3.9
-uv pip install torch tensorboard numpy tqdm pysc2 protobuf==3.20.2
+uv pip install torch tensorboard numpy==2.0 tqdm pysc2 protobuf==3.20.2
 
 python run_sl.py --workspace_path ~/AlphaStar_Implementation/ --model_name fullyconv --training True --gpu_use True --learning_rate 0.0001 --replay_hkl_file_path ~/AlphaStar_Implementation/pkl/h/ --environment Simple64
